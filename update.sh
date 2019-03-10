3fe0f024cd9327c7cb24623d90c077272e69f0bf
Mohamed Haneef <antikilerz@gmail.com>
2019-03-10 01:23:14 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 rm Packages.bz2
-dpkg-scanpackages -m debs /dev/null >Packages
+dpkg-scanpackages -m ./debs /dev/null >Packages
 bzip2 Packages
+
+
+#!/bin/bash
+dpkg-scanpackages ./debs > Packages
+bzip2 -fks Packages
+
