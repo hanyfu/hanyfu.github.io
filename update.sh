3291f3c5832c99e022180fd56db3715e61117aac
Mohamed Haneef <hanyfu@hotmail.com>
2019-03-10 23:17:06 -0700

    d

--- update.sh
+++ update.sh
@@ -1,13 +1,12 @@
-echo Removing Old Compressed Packages File
-rm -r Packages
-rm -r Packages.bz2
-rm -r Packages.gz
+# Create Packages files
+./dpkg-scanpackages -m debs /dev/null > Packages
+rm Packages.gz Packages.bz2
+gzip -c9 Packages > Packages.gz
+bzip2 -c9 Packages > Packages.bz2
 
-
-
-echo Creating New Packages file
-dpkg-scanpackages ./debs >Packages
-bzip2 -fks Packages
-
-echo Compressing New Packages List
-gzip -c Packages > Packages.gz
+# Upload to github with [message]
+echo "git commit message:"
+read message
+git add -A
+git commit -m "$message"
+git push -u origin master
